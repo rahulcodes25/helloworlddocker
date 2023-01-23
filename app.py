113704f0546336fb4bda087e41e97b5c5671d521
RahulJain <rj.rahul251998@gmail.com>
2023-01-23 13:45:22 +0530

    firstdockerfile

---
+++ app.py
@@ -0,0 +1,18 @@
+import time
+from flask import flask
+
+app = flask(__name__)
+
+start = time.time()
+
+def elapsed():
+    running = time.time() - start
+    minutes, seconds = divmod(running, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+@app.route("/")
+def route():
+    return "HEllo All (up %s)\n" % elapsed()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0",port=8080)
